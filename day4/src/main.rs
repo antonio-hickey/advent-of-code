41bb82f35f47a7b6851f3dc9eac86c0351ddc2d0
Antonio Hickey <antoniohickey99@gmail.com>
2022-12-04 19:28:56 +0000

    refactooor solution for part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,5 +1,12 @@
 fn main() { 
-    // Vector where the value is 1 if bad pair else is 0
+    println!("Part 1 Answer: {}", solution(1));
+    println!("Part 2 Answer: {}", solution(2));
+}
+
+
+fn solution(part: i8) ->i32 {
+    // Outputs a vector where an element 
+    // is 1 if overlap else element is 0
     let elve_pair_is_bad = include_str!("./input.prod")
         .split("\n")
         .filter(|&x| !x.is_empty())
@@ -16,15 +23,25 @@ fn main() {
                 .collect::<Vec<i32>>();
             elf_two_sections = (elf_two_sections[0]..elf_two_sections[1] + 1).collect::<Vec<i32>>();
 
-            if elf_one_sections.first() <= elf_two_sections.first() 
-                && elf_one_sections.last() >= elf_two_sections.last() 
-            || elf_two_sections.first() <= elf_one_sections.first()
-                && elf_two_sections.last() >= elf_one_sections.last()
-            { return 1 };
-
+            if part == 1 {
+                // Part one solution (total overlap of sections)
+                if elf_one_sections.first() <= elf_two_sections.first() 
+                    && elf_one_sections.last() >= elf_two_sections.last() 
+                || elf_two_sections.first() <= elf_one_sections.first()
+                    && elf_two_sections.last() >= elf_one_sections.last()
+                    { return 1 } 
+            } else {
+                // Part two solution (partial overlap of sections)
+                for section in elf_one_sections.iter() {
+                    if elf_two_sections.contains(section) {
+                        return 1
+                    } 
+                }
+            }
             return 0;
         })
         .collect::<Vec<i32>>();
-    
-    println!("Part 1 Answer: {}", elve_pair_is_bad.iter().sum::<i32>());
+
+    elve_pair_is_bad.iter().sum::<i32>()
 }
+
