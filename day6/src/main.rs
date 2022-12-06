ef7abb2022f6f02fbf70fcfc13de4cd5a6854d5f
Antonio Hickey <antoniohickey99@gmail.com>
2022-12-06 17:22:09 -0500

    (day 6): part two solution (ez pz)

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,22 +3,37 @@ fn main() {
         .filter(|&c| c != '\n' as char)
         .collect::<Vec<char>>();
 
-    println!("Part 1 Answer: {}", solution(&stream));
-    println!("Part 2 Answer: UNSOLVED");
+    println!("Part 1 Answer: {}", solution(&stream, 1));
+    println!("Part 2 Answer: {}", solution(&stream, 2));
 }
 
-fn solution(stream: &Vec<char>) -> usize {
+fn solution(stream: &Vec<char>, part: i8) -> usize {
     // Bruteforce solution (come back to refactor)
-    'next_window: for (idx, window) in stream.windows(4).enumerate() {
-        for i in 0..3 {
-            for j in i + 1..4 {
+    
+
+    // The solution doesn't have to change just the size of the window
+    // we use to iterate and search over.
+    let n: usize = if part == 1 {
+        // Part one looks for the start of a packet in the datastream
+        // which is indicated by 4 unique characters.
+        4
+    } else {
+        // Part two looks for the start of a message in the datastream 
+        // which is indicated 14 unique characters.
+        14
+    };
+
+
+    'next_window: for (idx, window) in stream.windows(n).enumerate() {
+        for i in 0..(n - 1) {
+            for j in i + 1..n {
                 if window[i] == window[j] {
                     continue 'next_window
                 }
             }
         }
 
-        return idx + 4;
+        return idx + n;
     }
     
     // Should only be hit if Advent of Code gave bad input
