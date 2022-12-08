1f888bc6adedd903a5cac2dc6ce77af19cd2a5ec
Antonio Hickey <antoniohickey99@gmail.com>
2022-12-08 12:02:25 -0500

    (day 7): part two solution

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -4,32 +4,35 @@ use std::rc::Rc;
 
 #[derive(Debug, Default)]
 struct Directory {
-    name: String,
+    _name: String,
     size: RefCell<usize>,
     parent: Option<Rc<Directory>>,
     subdir: RefCell<HashMap<String, Rc<Directory>>>,
+}
+impl Directory {
+    fn get_size(&self) -> usize {
+        *self.size.borrow() + self
+            .subdir.borrow().values()
+            .fold(0, |x, y| x + y.get_size())
+    }
 }
 
 fn main() {
     // Ewww this problem was hard in Rust, but learnt a lot about refrences :)
 
-    let input = include_str!("./input.test");
+    let input = include_str!("./input.prod");
 
+    println!("Part One Answer: {}", solution(input, 1));
+    println!("Part Two Answer: {}", solution(input, 2));
+}
+
+fn solution(input: &str, part: i8) -> usize {
     let root_directory = Rc::new(Directory {
-        name: String::from("/"),
+        _name: String::from("/"),
         size: RefCell::new(0),
         parent: None,
         subdir: RefCell::new(HashMap::new()),
     });
-
-    println!("Part One Answer: {}", solution(input, &root_directory));
-}
-
-fn get_size(dir: &Rc<Directory>) -> usize {
-    *dir.size.borrow() + dir.subdir.borrow().values().fold(0, |a, b| a + get_size(b))
-}
-
-fn solution(input: &str, root_directory: &Rc<Directory>) -> usize {
     let mut cwd = Rc::clone(&root_directory);
 
     for line in input.lines() {
@@ -52,7 +55,7 @@ fn solution(input: &str, root_directory: &Rc<Directory>) -> usize {
                 cwd.subdir.borrow_mut().insert(
                     String::from(directory_name),
                     Rc::new(Directory { 
-                        name: String::from(directory_name), 
+                        _name: String::from(directory_name), 
                         size: RefCell::new(0), 
                         parent: Some(Rc::clone(&cwd)), 
                         subdir: RefCell::new(HashMap::new()),
@@ -65,17 +68,40 @@ fn solution(input: &str, root_directory: &Rc<Directory>) -> usize {
         }
     }
 
+    // Part two variables
+    let total_space = &root_directory.get_size();
+    let free_space = 70_000_000 - total_space;
+    let space_needed = 30_000_000 - free_space;
+
     let mut to_visit = vec![Rc::clone(&root_directory)];
+
+    // Initialize Part one output 
+    // (total size of all directories under or equal to 100,000)
     let mut total = 0;
+
+    // Part two output 
+    // (the smallest directory that is greater or equal to space_needed)
+    let mut best = usize::MAX;
+
 
     while let Some(directory) = to_visit.pop() {
         to_visit.extend(directory.subdir.borrow().values().map(Rc::clone));
+        let size = &directory.get_size();
 
-        let size = get_size(&directory);
-        if size <= 100000 {
+        // Part one solution
+        if part == 1 && size <= &usize::try_from(100_000).unwrap() {
             total += size;
+        }
+
+        // Part two solution
+        if part == 2 && size >= &space_needed {
+            best = best.min(*size);
         }
     }
 
-    return total;
+    if part == 1 {
+        return total
+    } else {
+        return best
+    }
 }
