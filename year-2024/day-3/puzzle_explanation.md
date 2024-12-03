a45750d856143c38a3b6f865484ad4b5bd72582a
Antonio Hickey <antoniohickey99@gmail.com>
2024-12-03 14:53:08 -0500

    solution(2024): day 3 - part 1

--- year-2024/day-3/puzzle_explanation.md
+++ year-2024/day-3/puzzle_explanation.md
@@ -15,8 +15,10 @@ However, because the program's memory has been corrupted, there are also many in
 
 For example, consider the following section of corrupted memory:
 
-x**mul(2,4)**%&mul[3,7]!@^do_not_**mul(5,5)**+mul(32,64]then(**mul(11,8)****mul(8,5)**)
+```x**mul(2,4)**%&mul[3,7]!@^do_not_**mul(5,5)**+mul(32,64]then(**mul(11,8)****mul(8,5)**)```
 
 Only the four highlighted sections are real `mul` instructions. Adding up the result of each instruction produces **161** (`2*4 + 5*5 + 11*8 + 8*5`).
 
 Scan the corrupted memory for uncorrupted `mul` instructions. **What do you get if you add up all of the results of the multiplications?**
+
+- [My Solution](https://github.com/antonio-hickey/advent-of-code/blob/29cae2359b9ca0946e230b1790989f67a63600db/year-2024/day-3/src/main.rs#L13-L83), This one is really just a parsing puzzle 😒. I loop through the corrupted memory collecting mul instructions as long as they fit certain conditions. First I go through each occurrence of "mul" and check the size of it. The max parameter size is 3 digits so 999 meaning the max length of a valid mul instruction is 12 `mul(999,999)` = 12 chars and the min length is `mul(1,1)` = 8 chars. Then I grab the parameter space of the instruction (the digits in between "(" and ")" ex: `"999,999"`), and split that section by `","` and try to parse them into integers as if they don't parse then it's not a valid instruction. When finished I have a vector of all the valid mul op's and can just compute the product of each one and then sum up all those products for an answer to the puzzle.
--- year-2024/day-3/src/main.rs
+++ year-2024/day-3/src/main.rs
@@ -11,10 +11,55 @@ fn main() {
 
 /// Function to solve for both parts of the puzzle
 fn solution(input: &str, part: i8) -> i32 {
+    let computer_memory = ComputerMemory::from_puzzle_input(input);
+    let corrupted_memory = computer_memory.corrupted_memory;
+
     match part {
         1 => {
             /* Part One Solution */
-            todo!()
+            let mut valid_mul_ops: Vec<(i32, i32)> = Vec::new();
+            let mut current_instruction_idx = 0;
+
+            loop {
+                let remaining_instructions = &corrupted_memory[current_instruction_idx..];
+                if !remaining_instructions.contains("mul(") {
+                    break;
+                }
+
+                let next_instruction_start = remaining_instructions
+                    .find("mul")
+                    .expect("AoC not to have bad data");
+                let next_instruction_end = remaining_instructions[next_instruction_start..]
+                    .find(')')
+                    .expect("AoC not to have bad data")
+                    + next_instruction_start;
+
+                let possible_instruction =
+                    &remaining_instructions[next_instruction_start..=next_instruction_end];
+
+                if possible_instruction.len() >= 8 || possible_instruction.len() <= 12 {
+                    if let Some(param_start) = possible_instruction.find('(') {
+                        let param_end = possible_instruction.find(')').unwrap();
+                        let param_space = &possible_instruction[param_start + 1..param_end];
+                        if let Some(params_split) = param_space.split_once(',') {
+                            if let (Ok(param_1), Ok(param_2)) =
+                                (params_split.0.parse::<i32>(), params_split.1.parse::<i32>())
+                            {
+                                valid_mul_ops.push((param_1, param_2));
+                                current_instruction_idx += next_instruction_end;
+                                continue;
+                            }
+                        }
+                    }
+                }
+
+                current_instruction_idx += next_instruction_start + 3;
+            }
+
+            valid_mul_ops
+                .iter()
+                .map(|mul_instruction| mul_instruction.0 * mul_instruction.1)
+                .sum()
         }
         2 => {
             /* Part Two Solution */
@@ -24,13 +69,16 @@ fn solution(input: &str, part: i8) -> i32 {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 /// A model of the puzzle input data
-struct DataModel;
-impl DataModel {
+struct ComputerMemory {
+    corrupted_memory: String,
+}
+impl ComputerMemory {
     /// Parse the puzzle input data into a meaningful model
     fn from_puzzle_input(puzzle_data: &str) -> Self {
-        todo!()
+        ComputerMemory {
+            corrupted_memory: puzzle_data.to_string(),
+        }
     }
 }
-
