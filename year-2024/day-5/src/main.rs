81366edc9983e3a76cac959fcefad034662e5039
Antonio Hickey <contact@antoniohickey.com>
2024-12-05 21:57:10 -0500

    new puzzle: day 5

---
+++ year-2024/day-5/src/main.rs
@@ -0,0 +1,36 @@
+/* Day - 5: Print Queue */
+
+use std::fs;
+
+fn main() {
+    // Read puzzle input into string
+    let input = fs::read_to_string("./puzzle_data.txt").expect("some puzzle input data");
+    println!("part one solution: {}", solution(&input, 1));
+    println!("part two solution: {}", solution(&input, 2));
+}
+
+/// Function to solve for both parts of the puzzle
+fn solution(input: &str, part: i8) -> i64 {
+    // parse the puzzle input into a useful model
+    match part {
+        1 => {
+            /* Part One Solution */
+            todo!()
+        }
+        2 => {
+            /* Part Two Solution */
+            todo!()
+        }
+        _ => panic!("Only 2 parts to the puzzle broooo"),
+    }
+}
+
+#[derive(Debug)]
+/// A model of the puzzle input data
+struct DataModel;
+impl DataModel {
+    /// Parse the puzzle input data into a meaningful model
+    fn from_puzzle_input(puzzle_data: &str) -> Self {
+        todo!()
+    }
+}
